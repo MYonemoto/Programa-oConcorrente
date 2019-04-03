a60a1863366a47c29cb11433ff1e0fe671edeedd
Mateus Yonemoto <mateustomoo@gmail.com>
2019-04-02 21:02:36 -0300

    1 2 3 e 4 feitos

---
+++ ProgramacaoConcorrente/src/Topico3AtividadesSlide28/Atividade3.java
@@ -0,0 +1,42 @@
+/*
+Programação Concorrente
+Aluno: Mateus Tomoo Yonemoto Peixoto
+RA: 1602055
+
+    Atividade 3. Faça um programa em Java com threads que exiba os
+números primos entre 0 e 100000.
+ */
+package Topico3AtividadesSlide28;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Atividade3 implements Runnable {
+
+    @Override
+    public void run() {
+        List<Integer> numeros = new ArrayList<Integer>();
+        for (int i = 0; i <= 1000; i++) {
+            numeros.add(i);
+        }
+        int contador;
+        int numero = 0;
+        for (Integer i : numeros) {
+            contador = 0;
+            for (int u = 1; u <= i; u++) {
+                if (i % u == 0) {
+                    contador++;
+                    numero = u;
+                }
+            }
+            if (contador == 2) {
+                System.out.println("O Número: " + numero + " é primo e seus primos são: 1 e " + numero);
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        Thread t = new Thread(new Atividade3());
+        t.start();
+    }
+}
---
+++ ProgramacaoConcorrente/src/Topico3AtividadesSlide28/Atividade5.java
@@ -0,0 +1,17 @@
+/*
+Programação Concorrente
+Aluno: Mateus Tomoo Yonemoto Peixoto
+RA: 1602055
+
+    Atividade 5. Faça um programa multithreaded em Java que ordene um
+vetor usando o Merge Sort recursivo. Faça com que a thread
+principal dispare duas threads para classificar cada metade do
+vetor.
+
+
+ */
+package Topico3AtividadesSlide28;
+
+public class Atividade5 {
+    
+}
